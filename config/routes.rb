20b5a22b76c6eb95566e1d4ce2e38ccb1dd8b048
Ney Pimenta <ney@redealumni.com>
2017-05-27 20:57:55 -0300

    Auth0 wip

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   root 'welcome#index'
+  get 'dashboard' => 'dashboard#show'
+  get '/auth/auth0/callback' => 'auth0#callback'
+  get '/auth/failure'        => 'auth0#failure'
 end
