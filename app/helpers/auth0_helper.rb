7c04012c522edecb03f4b4ceaf9dc3abf64e7394
Ney Pimenta <ney@redealumni.com>
2017-05-28 12:52:27 -0300

    Attempt to fix login

--- app/helpers/auth0_helper.rb
+++ app/helpers/auth0_helper.rb
@@ -13,9 +13,6 @@ module Auth0Helper
 
   # Set the @current_user or redirect to public page
   def authenticate_user!
-    if Rails.env.development?
-      return
-    end
     # Redirect to page that has the login here
     if user_signed_in?
       @current_user = User.find_by(client_id: client_id)
@@ -28,7 +25,7 @@ module Auth0Helper
   # What's the current_user?
   # @return [Hash]
   def current_user
-    @current_user || User.last
+    @current_user
   end
 
   # @return the path to the login page
