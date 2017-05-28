2408e948b75a21f03380edc0cca584fd6f583701
Ney Pimenta <ney@redealumni.com>
2017-05-28 05:24:10 -0300

    Prizes page WIP

--- app/controllers/welcome_controller.rb
+++
@@ -1,6 +0,0 @@
-class WelcomeController < ApplicationController
-
-  def index
-  	redirect_to home_path if user_signed_in?
-  end
-end
--- app/models/prize.rb
+++ app/models/prize.rb
@@ -1,3 +1,6 @@
 class Prize < ApplicationRecord
 	has_and_belongs_to_many :users
+
+  scope :available, -> { where('quantity > 0') }
+
 end
