6c621222c8a5b2ef246fa61888e41053442c69f1
Marcos Costa Pinto <costa.marcos.pro@gmail.com>
2017-05-28 03:57:36 -0300

    Adds timeline helper

--- app/models/user_reward.rb
+++ app/models/user_reward.rb
@@ -2,6 +2,8 @@ class UserReward < ApplicationRecord
   belongs_to :rewarded_user, class_name: 'User'
   has_many :rewarding_users
 
+  scope :newest_first, -> { order(created_at: :desc) }
+
   def self.reward(from:, to:, description:, points:)
     if points > 0 && from.can_give?(points)
       reward = create(description: description, rewarded_user: to)
